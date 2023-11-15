384e93d5196630a580801c840971148713e54fd8
Catalina Nica <mariacatalina27.nica@gmail.com>
2023-11-15 12:38:16 +0200

    Tabele Roles si Users

---
+++ CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollectiveKnowledgePlatform.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+    }
+}
