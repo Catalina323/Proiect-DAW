77476665fdca34755908933d98889232285fb6b6
Catalina Nica <mariacatalina27.nica@gmail.com>
2023-11-15 13:17:49 +0200

    Actualizare model Role

--- CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/Role.cs
+++ CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/Role.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 using System.ComponentModel.DataAnnotations;
 
 namespace CollectiveKnowledgePlatform.Models
@@ -14,21 +13,4 @@ namespace CollectiveKnowledgePlatform.Models
         public virtual ICollection<User> Users { get; set; }
 
     }
-=======
-using System.ComponentModel.DataAnnotations;
-
-namespace CollectiveKnowledgePlatform.Models
-{
-    public class Role
-    {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        public virtual ICollection<User> Users { get; set; }
-
-    }
->>>>>>> Stashed changes
 }
