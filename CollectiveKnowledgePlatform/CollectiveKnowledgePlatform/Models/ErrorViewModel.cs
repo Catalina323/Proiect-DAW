1971c07a57725d3495e585d3bc9d4635e2c7f536
Catalina Nica <mariacatalina27.nica@gmail.com>
2023-11-15 11:54:35 +0200

    creare proiect

---
+++ CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CollectiveKnowledgePlatform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
