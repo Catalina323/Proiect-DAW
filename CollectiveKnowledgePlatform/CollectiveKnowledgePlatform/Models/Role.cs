94129550501162a74a773c76980d9d4fff2c852d
Catalina Nica <mariacatalina27.nica@gmail.com>
2023-11-15 13:08:45 +0200

    model TopicLike

--- CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/Role.cs
+++ CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/Role.cs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace CollectiveKnowledgePlatform.Models
-{
-    public class Role
-    {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        public string Name { get; set; }
-
-        public virtual ICollection<User> Users { get; set; }
-
-    }
-}
-=======
-
 using System.ComponentModel.DataAnnotations;
 
 namespace CollectiveKnowledgePlatform.Models
@@ -33,4 +14,3 @@ namespace CollectiveKnowledgePlatform.Models
 
     }
 }
->>>>>>> 8ccef598eeb137382678cc4ebbbc893d84ace551
--- CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/TopicLike.cs
+++ CollectiveKnowledgePlatform/CollectiveKnowledgePlatform/Models/TopicLike.cs
@@ -1,6 +1,19 @@
-﻿namespace CollectiveKnowledgePlatform.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CollectiveKnowledgePlatform.Models
 {
     public class TopicLike
     {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public virtual User User {  get; set; }
+
+        public int TopicId { get; set; }
+        public virtual Topic Topic { get; set; }
+
+        public int type { get; set; }
+
     }
 }
